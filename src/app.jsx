608ff9eb3ad65c3f2c738b7627afc8bb774ffa14
Sanjay S Kumar <35188255+Quantaindew@users.noreply.github.com>
2024-10-21 01:02:07 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -16,7 +16,7 @@ export function App() {
           <img src={preactLogo} class="logo preact" alt="Preact logo" />
         </a>
       </div>
-      <h1>Vite + CICD + JO + Preact</h1>
+      <h1>Vite + CICD + hello permaweb + Preact</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
